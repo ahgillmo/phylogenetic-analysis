5aaa17b74b99cc1c1bf74175bd4eb429b2a27f00
ahgillmo <ahgillmo@ucalgary.ca>
2024-05-23 19:29:53 -0600

    Update multisample_Mutect_to_pairtree_SSM.sh

--- multisample_Mutect_to_pairtree_SSM.sh
+++ multisample_Mutect_to_pairtree_SSM.sh
@@ -19,19 +19,19 @@
 
 # Step 1: Split the initial MuTect file into N samples
 # For splitting the XXXX.Merged.Filtered.leftnormalized.Mutect2.vcf.gz file
-# This loop processes each VCF file and splits it by sample
-# for file in *.vcf*; do 
-#     for sample in `bcftools query -l $file`; do 
-#         bcftools view -c1 -Oz -s $sample -o ${file/.vcf*/.$sample.vcf.gz} $file
-#     done 
-# done
+# This loop processes each VCF file and splits it into sample
+ for file in *.vcf*; do 
+     for sample in `bcftools query -l $file`; do 
+         bcftools view -c1 -Oz -s $sample -o ${file/.vcf*/.$sample.vcf.gz} $file
+     done 
+ done
 
 # Echoing the input files
 echo -e "The Mutect2 SNV/INDEL variant file is: $1"
 echo -e "The ControlFreec p-value segment file is: $2"
 
 # Process the ControlFreec segments to produce the most certain segments and add in unaffected (diploid) regions
-bash /home/ahgillmo/master_scripts_slurm/controlFreeC_scripts/process_controlfreec_segments_for_pairtree.sh $2
+bash process_controlfreec_segments_for_pairtree.sh $2
 
 # Create a variant list with labels "Variant" "ALT-count" "Depth" "CN_A" "CN_B"
 bedtools intersect -wa -wb -a <(zless $1 | egrep -v '#' | awk '$7=="PASS"' | sed 's/ /\t/g' | awk '{print $1"\t"$2"\t"$2"\t"$4"\t"$5"\t"$10}') -b <(zless $2".MostCertain_and_Significant_Segments_with_NormalRegionAdded.seg.tsv" | egrep -v Start | sed 's/ /\t/g') | \
@@ -40,7 +40,7 @@ awk '{print $1"\t"$5"\t"$2"\t"$3}' | sed 's/,/\t/g' | \
 awk '{print $1"\t"$3"\t"($3+$2)"\t"$4"\t"$5}' | sort -u > $1".temp.PairTree.txt"
 
 # Calculate the variant read probability required for PairTree
-bash /home/ahgillmo/master_scripts_slurm/pairtree_scripts/Calc_read_probability.sh $1".temp.PairTree.txt"
+bash Calc_read_probability.sh $1".temp.PairTree.txt"
 
 # Clean up the temporary files
 rm $2".MostCertain_and_Significant_Segments_with_NormalRegionAdded.seg.tsv"
